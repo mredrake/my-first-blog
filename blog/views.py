4d0c020e5a0252300a47c39ec1e3b65b7af52751
mredrake <utkinilya@yandex.ru>
2017-03-25 21:17:47 +0300

    with empty station

--- blog/views.py
+++ blog/views.py
@@ -5,6 +5,7 @@ from django.utils import timezone
 
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
-    return render(request, 'blog/post_list.html', {'posts': posts})
+    posts_exist = posts or False and True
+    return render(request, 'blog/post_list.html', {'posts': posts, 'posts_exist': posts_exist})
 
 # Create your views here.
