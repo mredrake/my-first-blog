60fbd0a0ccf5d7eba155c42c4387d0fde49d4bee
mredrake <utkinilya@yandex.ru>
2017-04-19 16:41:13 +0300

    Update urls.py

--- mysite/urls.py
+++ mysite/urls.py
@@ -15,13 +15,14 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
-from django.contrib.sitemaps import views
+from django.contrib.sitemaps.views import sitemap
+
+
 
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'', include('blog.urls')),
-    url(r'^sitemap\.xml$', views.index, {'sitemaps': sitemaps}),
-    url(r'^sitemap-(?P<section>.+)\.xml$', views.sitemap, {'sitemaps': sitemaps},
+    url(r'^sitemap\.xml$', sitemap, {'sitemaps': sitemaps},
         name='django.contrib.sitemaps.views.sitemap')
 ]
