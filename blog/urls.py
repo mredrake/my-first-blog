0599cf4ddac05901fb26104d755a141a8a868527
mredrake <utkinilya@yandex.ru>
2017-04-19 17:01:03 +0300

    Update urls.py

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,17 @@
 from django.conf.urls import url
 from . import views
+from django.contrib.sitemaps.views import sitemap
+from .sitemaps import StaticViewSitemap
+
+
+sitemaps = {
+    'static': StaticViewSitemap,
+}
 
 
 urlpatterns = [
    url(r'^$', views.post_list, name='post_list'),
-   url(r'^post/(?P<pk>[0-9]+)/$', views.post_detail, name = 'post_detail')
+   url(r'^post/(?P<pk>[0-9]+)/$', views.post_detail, name = 'post_detail'),
+   url(r'^sitemap\.xml$', sitemap, {'sitemaps': sitemaps},
+       name='django.contrib.sitemaps.views.sitemap')
 ]
