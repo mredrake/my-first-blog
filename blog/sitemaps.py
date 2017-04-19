b48dbefa44ecd5edb5df2df4178642169f02886a
mredrake <utkinilya@yandex.ru>
2017-04-19 17:28:30 +0300

    Update sitemaps.py

--- blog/sitemaps.py
+++ blog/sitemaps.py
@@ -1,9 +1,17 @@
 from django.contrib import sitemaps
 from django.core.urlresolvers import reverse
+from django.utils import timezone
+from .models import Post
 
 class StaticViewSitemap(sitemaps.Sitemap):
     priority = 0.5
     changefreq = 'daily'
     
     def items(self):
-        return['post_detail']
+        return Post.objects.filter(published_date__lte=timezone.now())
+    
+    def lastmod(self, Post):
+        return Post.published_date 
+
+    def location(self, Post):
+        return "/post/" + Post.pk + "/"
