dffb519b7439b83e49d303bc30cfa1a29b5bd505
mredrake <utkinilya@yandex.ru>
2017-04-19 16:53:42 +0300

    Update sitemaps.py

--- mysite/sitemaps.py
+++ mysite/sitemaps.py
@@ -5,8 +5,5 @@ class StaticViewSitemap(sitemaps.Sitemap):
     priority = 0.5
     changefreq = 'daily'
 
-    def items(self):
-        return ['main', 'about', 'license']
-
     def location(self, item):
         return reverse(item)
