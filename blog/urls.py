26fadb09fa32b4f9720675349a893880b38854ce
mredrake <utkinilya@yandex.ru>
2017-04-19 16:29:13 +0300

    revert

--- blog/urls.py
+++ blog/urls.py
@@ -1,12 +1,8 @@
 from django.conf.urls import url
 from . import views
-from django.contrib.sitemaps import views as stmpviews
 
 
 urlpatterns = [
    url(r'^$', views.post_list, name='post_list'),
-   url(r'^post/(?P<pk>[0-9]+)/$', views.post_detail, name = 'post_detail'),
-   url(r'^sitemap\.xml$', stmpviews.index, {'sitemaps': sitemaps}),
-   url(r'^sitemap-(?P<section>.+)\.xml$', stmpviews.sitemap, {'sitemaps': sitemaps},
-        name='django.contrib.sitemaps.views.sitemap')
+   url(r'^post/(?P<pk>[0-9]+)/$', views.post_detail, name = 'post_detail')
 ]
