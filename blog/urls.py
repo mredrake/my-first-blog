e8675ec56b2ab7471e9646b12d003f57ad618f79
mredrake <utkinilya@yandex.ru>
2017-04-19 16:19:10 +0300

    sitemap add

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,12 @@
 from django.conf.urls import url
 from . import views
+from django.contrib.sitemaps import views
+
 
 urlpatterns = [
    url(r'^$', views.post_list, name='post_list'),
-   url(r'^post/(?P<pk>[0-9]+)/$', views.post_detail, name = 'post_detail')
+   url(r'^post/(?P<pk>[0-9]+)/$', views.post_detail, name = 'post_detail'),
+   url(r'^sitemap\.xml$', views.index, {'sitemaps': sitemaps}),
+   url(r'^sitemap-(?P<section>.+)\.xml$', views.sitemap, {'sitemaps': sitemaps},
+        name='django.contrib.sitemaps.views.sitemap')
 ]
