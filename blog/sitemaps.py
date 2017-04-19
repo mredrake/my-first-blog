b9399f9036197465319f8890e6af43b0a7ed69aa
mredrake <utkinilya@yandex.ru>
2017-04-19 17:03:38 +0300

    Update sitemaps.py

--- blog/sitemaps.py
+++ blog/sitemaps.py
@@ -4,6 +4,9 @@ from django.core.urlresolvers import reverse
 class StaticViewSitemap(sitemaps.Sitemap):
     priority = 0.5
     changefreq = 'daily'
+    
+    def items(self):
+        return['post_detail']
 
     def location(self, item):
         return reverse(item)
