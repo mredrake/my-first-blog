0b0e4f82d37783bac43b88f6df842e88f38ad267
mredrake <utkinilya@yandex.ru>
2017-04-24 11:45:55 +0300

    Update urls.py

--- blog/urls.py
+++ blog/urls.py
@@ -5,6 +5,7 @@ from .sitemaps import DinamicSitemap, StaticSitemap
 from django.views.generic.base import RedirectView
 
 favicon_view = RedirectView.as_view(url='/static/favicon.ico', permanent=True)
+robots_view = RedirectView.as_view(url='/static/robots.txt', permanent=True)
 
 
 sitemaps = {'articles': DinamicSitemap, 'static': StaticSitemap}
@@ -14,6 +15,7 @@ sitemaps = {'articles': DinamicSitemap, 'static': StaticSitemap}
 urlpatterns = [
    url(r'^$', views.post_list, name='post_list'),
    url(r'^favicon\.ico$', favicon_view, name='favicon'),
+   url(r'^robots\.txt$', robots_view, name='robots'),
    url(r'^post/(?P<pk>[0-9]+)/$', views.post_detail, name = 'post_detail'),
    url(r'^sitemap\.xml$', sitemap, {'sitemaps': sitemaps},
        name='django.contrib.sitemaps.views.sitemap')
