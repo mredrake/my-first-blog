e2cdf2b34f80c83e2119b15ad25185a4836a3d94
mredrake <utkinilya@yandex.ru>
2017-04-24 11:51:43 +0300

    Update views.py

--- blog/views.py
+++ blog/views.py
@@ -12,3 +12,6 @@ def post_list(request):
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk = pk)
     return render(request, 'blog/post_detail.html', {'post': post})
+
+def robots(request):
+    return render(request, 'static/robots.txt')
