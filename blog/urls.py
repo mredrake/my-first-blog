457bb86cfab6b152eb75454e365e7c1a1470d58f
mredrake <utkinilya@yandex.ru>
2017-04-24 11:53:59 +0300

    Update urls.py

--- blog/urls.py
+++ blog/urls.py
@@ -14,6 +14,7 @@ sitemaps = {'articles': DinamicSitemap, 'static': StaticSitemap}
 urlpatterns = [
    url(r'^$', views.post_list, name='post_list'),
    url(r'^favicon\.ico$', favicon_view, name='favicon'),
+   url(r'^robots\.txt$', views.robots, name='robots'),
    url(r'^post/(?P<pk>[0-9]+)/$', views.post_detail, name = 'post_detail'),
    url(r'^sitemap\.xml$', sitemap, {'sitemaps': sitemaps},
        name='django.contrib.sitemaps.views.sitemap')
